69b526e83b1f7b5b613bbde8d9b07f66540adeb2
Alex-Cristian <craciunescua5@gmail.com>
2026-03-05 04:54:41 +0200

    merge resolved

--- bookings/apps.py
+++ bookings/apps.py
@@ -1,17 +1,5 @@
 from django.apps import AppConfig
-<<<<<<< HEAD
-=======
-
-
->>>>>>> origin/main
 class BookingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookings'
     verbose_name = 'Programări'
-<<<<<<< HEAD
-=======
-
-    def ready(self):
-        # Register signals (create notifications on booking events)
-        from . import signals  # noqa: F401
->>>>>>> origin/main
