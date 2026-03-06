68d1c2635055e27dfc0d95887f56d6a61175e5ee
diaconescustefan <diaconescustefan75@gmail.com>
2026-03-06 03:49:38 +0200

    harta

--- autohub/settings.py
+++ autohub/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import dj_database_url
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,10 +56,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'autohub.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
